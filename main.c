64d3dcbf272d62a4a9024ed64f105caff5b33683
Sparvh <37019968+Sparvh@users.noreply.github.com>
2018-03-04 19:31:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,34 @@
 #include <pic32mx.h>
 #include <stdint.h>
+#include "time4io.c"
 //#include <stdlib.h>
 //#include "mipslab.h"
 
 void user_isr(void);
 void init_mcu(void);
 void init_tim(void);
+int getsw (void);
+int getbtns(void);
 
 void init_display(void);
 void display_update(void);
 void display_cls(void);
+
+//Buttons
+int button;
+int button1;
+int switches;
+
+//Test
+int pixeltestx = 32;
+int pixeltesty = 2;
+
+int pixelP2x = 94;
+int pixelP2y = 2;
+
+//scores
+int Player1Score = 0;
+int Player2Score = 0;
 
 //player 1
 int player1x = 0;
@@ -27,6 +46,25 @@ int player2h = 8;
 int ballx = 64;
 int bally = 16;
 
+// Winning
+int temp1;
+int temp;
+int ten = 10;
+int eleven = 11;
+int twelve = 12;
+
+int slut1 = 0;
+int slut2 = 0;
+int gameMode = 0;
+int collisionThing = 1;
+
+char textbuffer[4][16];
+
+const uint8_t screen[128][4];
+uint8_t bufferstate[512];
+const uint8_t slut[5];
+
+
 #define DISPLAY_CHANGE_TO_COMMAND_MODE (PORTFCLR = 0x10)
 #define DISPLAY_CHANGE_TO_DATA_MODE (PORTFSET = 0x10)
 
@@ -39,18 +77,151 @@ int bally = 16;
 #define DISPLAY_TURN_OFF_VDD (PORTFSET = 0x40)
 #define DISPLAY_TURN_OFF_VBAT (PORTFSET = 0x20)
 
-// #define DISPLAY_VDD_PORT PORTF
-// #define DISPLAY_VDD_MASK 0x40
-// #define DISPLAY_VBATT_PORT PORTF // VBATT sätter på själva skärmen
-// #define DISPLAY_VBATT_MASK 0x20
-// #define DISPLAY_COMMAND_DATA_PORT PORTF
-// #define DISPLAY_COMMAND_DATA_MASK 0x10
-// #define DISPLAY_RESET_PORT PORTG
-// #define DISPLAY_RESET_MASK 0x200
-char textbuffer[4][16];
-const uint8_t screen[128][4];
-uint8_t bufferstate[512];
-const uint8_t slut[5];
+
+const uint8_t const font[] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 94, 0, 0, 0, 0,
+    0, 0, 4, 3, 4, 3, 0, 0,
+    0, 36, 126, 36, 36, 126, 36, 0,
+    0, 36, 74, 255, 82, 36, 0, 0,
+    0, 70, 38, 16, 8, 100, 98, 0,
+    0, 52, 74, 74, 52, 32, 80, 0,
+    0, 0, 0, 4, 3, 0, 0, 0,
+    0, 0, 0, 126, 129, 0, 0, 0,
+    0, 0, 0, 129, 126, 0, 0, 0,
+    0, 42, 28, 62, 28, 42, 0, 0,
+    0, 8, 8, 62, 8, 8, 0, 0,
+    0, 0, 0, 128, 96, 0, 0, 0,
+    0, 8, 8, 8, 8, 8, 0, 0,
+    0, 0, 0, 0, 96, 0, 0, 0,
+    0, 64, 32, 16, 8, 4, 2, 0,
+    0, 62, 65, 73, 65, 62, 0, 0,
+    0, 0, 66, 127, 64, 0, 0, 0,
+    0, 0, 98, 81, 73, 70, 0, 0,
+    0, 0, 34, 73, 73, 54, 0, 0,
+    0, 0, 14, 8, 127, 8, 0, 0,
+    0, 0, 35, 69, 69, 57, 0, 0,
+    0, 0, 62, 73, 73, 50, 0, 0,
+    0, 0, 1, 97, 25, 7, 0, 0,
+    0, 0, 54, 73, 73, 54, 0, 0,
+    0, 0, 6, 9, 9, 126, 0, 0,
+    0, 0, 0, 102, 0, 0, 0, 0,
+    0, 0, 128, 102, 0, 0, 0, 0,
+    0, 0, 8, 20, 34, 65, 0, 0,
+    0, 0, 20, 20, 20, 20, 0, 0,
+    0, 0, 65, 34, 20, 8, 0, 0,
+    0, 2, 1, 81, 9, 6, 0, 0,
+    0, 28, 34, 89, 89, 82, 12, 0,
+    0, 0, 126, 9, 9, 126, 0, 0,
+    0, 0, 127, 73, 73, 54, 0, 0,
+    0, 0, 62, 65, 65, 34, 0, 0,
+    0, 0, 127, 65, 65, 62, 0, 0,
+    0, 0, 127, 73, 73, 65, 0, 0,
+    0, 0, 127, 9, 9, 1, 0, 0,
+    0, 0, 62, 65, 81, 50, 0, 0,
+    0, 0, 127, 8, 8, 127, 0, 0,
+    0, 0, 65, 127, 65, 0, 0, 0,
+    0, 0, 32, 64, 64, 63, 0, 0,
+    0, 0, 127, 8, 20, 99, 0, 0,
+    0, 0, 127, 64, 64, 64, 0, 0,
+    0, 127, 2, 4, 2, 127, 0, 0,
+    0, 127, 6, 8, 48, 127, 0, 0,
+    0, 0, 62, 65, 65, 62, 0, 0,
+    0, 0, 127, 9, 9, 6, 0, 0,
+    0, 0, 62, 65, 97, 126, 64, 0,
+    0, 0, 127, 9, 9, 118, 0, 0,
+    0, 0, 38, 73, 73, 50, 0, 0,
+    0, 1, 1, 127, 1, 1, 0, 0,
+    0, 0, 63, 64, 64, 63, 0, 0,
+    0, 31, 32, 64, 32, 31, 0, 0,
+    0, 63, 64, 48, 64, 63, 0, 0,
+    0, 0, 119, 8, 8, 119, 0, 0,
+    0, 3, 4, 120, 4, 3, 0, 0,
+    0, 0, 113, 73, 73, 71, 0, 0,
+    0, 0, 127, 65, 65, 0, 0, 0,
+    0, 2, 4, 8, 16, 32, 64, 0,
+    0, 0, 0, 65, 65, 127, 0, 0,
+    0, 4, 2, 1, 2, 4, 0, 0,
+    0, 64, 64, 64, 64, 64, 64, 0,
+    0, 0, 1, 2, 4, 0, 0, 0,
+    0, 0, 48, 72, 40, 120, 0, 0,
+    0, 0, 127, 72, 72, 48, 0, 0,
+    0, 0, 48, 72, 72, 0, 0, 0,
+    0, 0, 48, 72, 72, 127, 0, 0,
+    0, 0, 48, 88, 88, 16, 0, 0,
+    0, 0, 126, 9, 1, 2, 0, 0,
+    0, 0, 80, 152, 152, 112, 0, 0,
+    0, 0, 127, 8, 8, 112, 0, 0,
+    0, 0, 0, 122, 0, 0, 0, 0,
+    0, 0, 64, 128, 128, 122, 0, 0,
+    0, 0, 127, 16, 40, 72, 0, 0,
+    0, 0, 0, 127, 0, 0, 0, 0,
+    0, 120, 8, 16, 8, 112, 0, 0,
+    0, 0, 120, 8, 8, 112, 0, 0,
+    0, 0, 48, 72, 72, 48, 0, 0,
+    0, 0, 248, 40, 40, 16, 0, 0,
+    0, 0, 16, 40, 40, 248, 0, 0,
+    0, 0, 112, 8, 8, 16, 0, 0,
+    0, 0, 72, 84, 84, 36, 0, 0,
+    0, 0, 8, 60, 72, 32, 0, 0,
+    0, 0, 56, 64, 32, 120, 0, 0,
+    0, 0, 56, 64, 56, 0, 0, 0,
+    0, 56, 64, 32, 64, 56, 0, 0,
+    0, 0, 72, 48, 48, 72, 0, 0,
+    0, 0, 24, 160, 160, 120, 0, 0,
+    0, 0, 100, 84, 84, 76, 0, 0,
+    0, 0, 8, 28, 34, 65, 0, 0,
+    0, 0, 0, 126, 0, 0, 0, 0,
+    0, 0, 65, 34, 28, 8, 0, 0,
+    0, 0, 4, 2, 4, 2, 0, 0,
+    0, 120, 68, 66, 68, 120, 0, 0,
+};
+void display_string(int line, char *s) {
+    int i;
+    if(line < 0 || line >= 4)
+        return;
+    if(!s)
+        return;
+    
+    for(i = 0; i < 16; i++)
+        if(*s) {
+            textbuffer[line][i] = *s;
+            s++;
+        } else
+            textbuffer[line][i] = ' ';
+}
 
 void quicksleep(int cyc) {
 	int i;
@@ -71,15 +242,8 @@ void setPixel(const short x, const short y, const uint8_t state) {
 	}
 }
 
-void init_mcu(void)
+void init_basic(void)
 {
-	/* Set up peripheral bus clock */
-	/* OSCCONbits.PBDIV = 1; */
-	OSCCONCLR = 0x100000; /* clear PBDIV bit 1 */
-	OSCCONSET = 0x080000; /* set PBDIV bit 0 */
-
-	/* Enable LED1 through LED8 */
-	TRISECLR = 0x00FF;
 
 	/* Configure switches and buttonpins as inputs */
 	TRISDSET = 0x0FE0; // Switches 4:1, Buttons 4:2
@@ -100,22 +264,6 @@ void init_mcu(void)
  	SPI2CONSET = 0x40; 		/* SPI2CON bit CKP = 1; */
 	SPI2CONSET = 0x20; 		/* SPI2CON bit MSTEN = 1; */
 	SPI2CONSET = 0x8000;	/* SPI2CON bit ON = 1; */
-}
-
-/* Init timer 2 with timeout interrupts */
-void init_tim(void)
-{
-	/* Configure Timer 2 */
-	T2CON     = 0x0;        	// reset control register
-	T2CONSET  = (0x4 << 4);     // set prescaler to 1:16
-	//PR2       = TMR2PERIOD; 	// set period length
-	TMR2      = 0;          	// reset timer value
-	T2CONSET  = (0x1 << 15);    // start the timer
-
-	/* Enable Timer 2 interrupts */
-	IPC(2)    = 0x4;        	// set interrupt priority to 4
-	IECSET(0) = 0x1<<8;      	// timer 2 interrupt enable
-	IFSCLR(0) = 0x1<<8;      	// clear timer 2 interrupt flag
 }
 
 uint8_t spi_send_recv(uint8_t data) {
@@ -155,6 +303,30 @@ void display_init(void) {
 	spi_send_recv(0xAF);
 }
 
+void display_update(void) {
+	int i, j, k;
+	int c;
+	for(i = 0; i < 4; i++) {
+		DISPLAY_CHANGE_TO_COMMAND_MODE;
+		spi_send_recv(0x22);
+		spi_send_recv(i);
+		
+		spi_send_recv(0x0);
+		spi_send_recv(0x10);
+		
+		DISPLAY_CHANGE_TO_DATA_MODE;
+		
+		for(j = 0; j < 16; j++) {
+			c = textbuffer[i][j];
+			if(c & 0x80)
+				continue;
+			
+			for(k = 0; k < 8; k++)
+				spi_send_recv(font[c*8 + k]);
+		}
+	}
+}
+
 void display_image(int x, const uint8_t *data) {
 	int i, j;
 	
@@ -175,61 +347,6 @@ void display_image(int x, const uint8_t *data) {
 }
 
 
-
-
-void move_ballplayer2(){ //Fixar så att bollen rör sig mot spelare 2
-		quicksleep(100000);
-		ballx += 1;
-		setPixel(ballx, bally, 1);
-}
-
-void move_ballplayer1(){ //Fixar så att bollen rör sig mot spelare 1
-		quicksleep(100000);
-		ballx -= 1;
-		setPixel(ballx, bally, 1);
-}
-
-void move_balldown(){//Fixar så att bollen kan röra sig neråt på skärmen
-		quicksleep(100000);
-		bally += 1;
-		setPixel(ballx, bally, 1);
-}
-
-void move_ballup(){ //Fixar så att bollen kan röra sig uppåt på skärmen
-		quicksleep(100000);
-		bally -= 1;
-		setPixel(ballx, bally, 1);
-}
-
-void ballColision1(){ //Ball colision for player 1
-	if (ballx == 3){ //The x-position where player 1 is
-		
-		if ( ballx <
-		
-	}
-}
-
-void ballColision2(){ //Ball colision for player 2
-}
-
-void ballMovement(){
-	
-}
-
-void move_player1(){
-	
-}
-
-void move_player2(){
-	
-}
-
-void Menuscreen(){
-}
-
-void Victoryscreen(){
-}
-
 void getRect(int x, int y, int w, int h, const uint8_t state){
 	
 	int i,j;
@@ -247,7 +364,227 @@ void getRect(int x, int y, int w, int h, const uint8_t state){
 	}
 }
 
-void clear(){
+
+void move_ballplayer2(){ //Fixar så att bollen rör sig mot spelare 2
+
+		if (switches == 1){
+		quicksleep(10000);
+		ballx += 1;
+		setPixel(ballx, bally, 1);
+		}
+		else {
+		quicksleep(100000);
+		ballx += 1;
+		setPixel(ballx, bally, 1);
+		}
+		
+}
+
+void move_ballplayer1(){ //Fixar så att bollen rör sig mot spelare 1
+
+		if (switches == 1){ //hardmode, makes it go faster
+		quicksleep(10000);
+		ballx -= 1;
+		setPixel(ballx, bally, 1);
+		}
+		 else {
+		quicksleep(100000);
+		ballx -= 1;
+		setPixel(ballx, bally, 1);
+		}
+}
+
+void move_balldown(){//Fixar så att bollen kan röra sig neråt på skärmen
+		if (switches == 1){ // hardmode, makes it go faster
+		quicksleep(10000);
+		bally += 1;
+		setPixel(ballx, bally, 1);
+			
+		}
+		else {
+		quicksleep(100000);
+		bally += 1;
+		setPixel(ballx, bally, 1);
+		}
+}
+
+void move_ballup(){ //Fixar så att bollen kan röra sig uppåt på skärmen
+		if (switches == 1){ //hardmode, makes it go faster
+		quicksleep(10000);
+		bally -= 1;
+		setPixel(ballx, bally, 1);
+		}
+		else{
+		quicksleep(100000);
+		bally -= 1;
+		setPixel(ballx, bally, 1);
+		}
+}
+
+void ballColision1(){ //Ball colision for player 1
+	if (ballx == 3){ //The x-position where player 1 is
+	
+	if (player1y < 16){
+		int p1 = 0;
+		int yvalue = player1y;
+		int yvalueball = bally;
+		
+		for (p1 = 0; p1 < 8; p1++){
+		if ( yvalueball == (yvalue + p1)){
+			//quicksleep(100000);
+			move_ballplayer2();
+			slut1 = 1;
+			break;
+		}
+		}
+	}
+	
+	}
+}
+
+void ballColision2(){ //Ball colision for player 2
+
+	if (ballx == 125){ //The x-position where player 2 is
+	
+	if (player2y < 16){
+		int p2 = 0;
+		int yvalue2 = player2y;
+		int yvalueball2 = bally;
+		
+		for (p2 = 0; p2 < 8; p2++){
+		if ( yvalueball2 == (yvalue2 + p2)){
+			
+			//quicksleep(100000);
+			move_ballplayer1();
+			slut1 = 0;
+			break;
+		}
+		}
+	}
+}
+}
+
+void move_player1up(){
+	if ( player1y >= 0 ){
+		
+	if (switches == 1){
+	quicksleep(10000);
+	player1y = player1y - 1;
+		
+	}
+	else{
+	quicksleep(100000);
+	player1y = player1y - 1;
+	}
+	}
+}
+
+void move_player1down(){
+	
+	if ( player1y < 26 ){
+	if (switches == 1){
+	quicksleep(10000);
+	player1y = player1y + 1;
+	}
+	else{
+	quicksleep(100000);
+	player1y = player1y + 1;
+	}
+	}
+}
+
+void move_player2up(){
+	if ( player2y >= 0 ){
+		
+	if (switches == 1){
+	quicksleep(10000);
+	player2y = player2y - 1;}
+	else{
+	quicksleep(100000);
+	player2y = player2y - 1;
+	}
+	}
+}
+void move_player2down(){
+	if ( player2y < 26 ){
+		
+	if (switches == 1){
+	quicksleep(10000);
+	player2y = player2y + 1;
+	}
+	else {
+	quicksleep(100000);
+	player2y = player2y + 1;
+	}
+	}
+	
+}
+
+void move_AI(){
+	
+		if (bally > player2y){
+			player2y = player2y + 1;
+		}
+		else{
+			player2y = player2y - 1;
+		}
+		
+}
+
+void BallCollisionWall2(){ //Om bollen är längst uppe på skärmen påväg mot player 2
+	if (bally == 0){
+		move_ballplayer2();
+		move_balldown();
+		collisionThing = 1;
+		
+	}
+	
+}
+void BallCollisionWall22(){ //Om bollen är längst nere på skärmen påväg mot player 2
+	if (bally == 32){
+		move_ballplayer2();
+		move_ballup();
+		collisionThing = 2;
+	}
+}
+
+void BallCollisionWall1(){ //Om bollen är längst uppe på skärmen påväg mot player 1
+	
+		if (bally == 0){
+		move_ballplayer1();
+		move_balldown();
+		collisionThing = 1;
+		}
+	
+}
+
+void BallCollisionWall11(){ //Om bollen är längst nere på skärmen påväg mot player 1
+	
+		if (bally == 32){
+		move_ballplayer1();
+		move_ballup();
+		collisionThing = 2;
+		}
+}
+
+int getsw (void) { //Switcharna
+	
+	int switches = (PORTD >> 8) & 0x000f;
+	return switches;
+}
+
+int getbtns(void) {	//Input för knapp 4-2
+	
+	int button = (PORTD >> 5) & 0x0007;
+	return button;
+}
+
+int getbtn1(void){ //Inputen för knapp1
+	int button1 = (PORTF >> 1) & 0x0001;
+	return button1;
+}
+
+void clear(){ //Clear screen
 	
 	int i;
 	
@@ -257,39 +594,197 @@ void clear(){
 	
 }
 
+int Player1ScoreBoard(int x)
+{
+	if (x > 0){
+	switch(1){
+	
+	case 1:
+	setPixel(pixeltestx, pixeltesty, 1);
+	setPixel(pixeltestx, pixeltesty + 1, 1);
+	setPixel(pixeltestx, pixeltesty + 2, 1);
+	setPixel(pixeltestx, pixeltesty + 3, 1);
+	
+	if (x <= 1){
+		break;
+	}
+	case 2:
+	setPixel(pixeltestx + 2, pixeltesty, 1);
+	setPixel(pixeltestx + 2, pixeltesty + 1, 1);
+	setPixel(pixeltestx + 2, pixeltesty + 2, 1);
+	setPixel(pixeltestx + 2, pixeltesty + 3, 1);
+	}}
+
+}
+
+int Player2ScoreBoard(int x)
+{
+	if (x > 0){
+	switch(1){
+	
+	case 1:
+	setPixel(pixelP2x, pixelP2y, 1);
+	setPixel(pixelP2x, pixelP2y + 1, 1);
+	setPixel(pixelP2x, pixelP2y + 2, 1);
+	setPixel(pixelP2x, pixelP2y + 3, 1);
+	
+	if (x <= 1){
+		break;
+	}
+	case 2:
+	setPixel(pixelP2x + 2, pixelP2y, 1);
+	setPixel(pixelP2x + 2, pixelP2y + 1, 1);
+	setPixel(pixelP2x + 2, pixelP2y + 2, 1);
+	setPixel(pixelP2x + 2, pixelP2y + 3, 1);
+	}}
+
+}
+
 int main(void){
 	
-init_mcu(); //Initilize led lampor och basic osv
+init_basic(); //Initilize led lampor och basic osv
 display_init(); //Initilize displayen 
+TRISD = 0x0fe0;
 
-while (1){
+int GAME = 0;
+
+while (GAME == 0){
+	int butten;
+	butten = getbtns();
+	display_string(0,"press 1");
+	display_string(1,"for 1 player");
+	display_string(2,"press 2");
+	display_string(3,"for 2 player");
+	display_update();
+	
+	if ( butten == 2 ){ // Går direkt till spelet (multi)
+		GAME = 1;
+	}
+	if ( butten == 4){ //Kör singleplayer mode
+		GAME = 1;
+		gameMode = 1;
+	}
+	
+}
+
+while (GAME == 1){ //Actual game running
+	
+	if (Player1Score == 3){ //Om player1 vinner, gå till player1 victoryscreen
+		GAME = 2;
+	}
+	
+	if (Player2Score == 3) { //Om player2 vinner, gå till player2 Victoryscreen
+		GAME = 3;
+	}
+	
+	else { // Else, kör spelet
+	
 	clear();
+	button = getbtns();
+	switches = getsw();
+	button1 = getbtn1();
+	Player1ScoreBoard(Player1Score); //checking to update score
+	Player2ScoreBoard(Player2Score); 
+	
+	if (ballx == 0){ //Check for score for player 2
+	Player2Score = Player2Score + 1;
+		ballx = 64;
+		bally = 16;
+	}
+	
+	if (ballx == 128){ //Check for score for player 1
+		Player1Score = Player1Score + 1;
+		ballx = 64;
+		bally = 16;
+	}
+	
+	
 	getRect(player1x,player1y,player1w,player1h,1); //Skapar spelare 1
 	
 	getRect(player2x,player2y,player2w,player2h,1); //skapar spelare 2
 	
 	setPixel(ballx, bally, 1); //Sätter ut bollen i mitten
 	
+	ballColision1(); //Constantly check collision
+	ballColision2();
 	
-	// if (ballx < 128 || ballx > 0){
-		// quicksleep(100000);
-		// ballx += 1;
-		// setPixel(ballx, bally, 1);
-	// }
 	
-	if (bally < 0 )
 	
-	move_ballplayer1();
-	move_ballup();
+	if (button == 4){
+		move_player1up();
+	}	
+	if (button == 2) {
+		move_player1down();
+	}
+	
+	
+	if (gameMode == 0){ //If its multiplayer mode
+	if (button1 == 1){
+	move_player2down();
+	}
+	if (button == 1){
+	move_player2up();
+	}
+	}
+	if (gameMode == 1) { //If it's singlepalyer
+		move_AI(); //Calls for the AI constantly
+	}
+	
+	if (slut1 == 1){ //If it's succesfully colided with player 1
+		
+		BallCollisionWall2(); // Keepchecking collision
+		BallCollisionWall22();
+		
+		if (collisionThing == 1){ // Checking if it's on the top part of screen
+			move_ballplayer2();
+			move_balldown();
+		}
+		
+		if (collisionThing == 2){ //Checking if it's on the bottom part of screen
+			move_ballplayer2();
+			move_ballup();			
+		}
+	
+	}
+	
+	if (slut1 == 0){ //If it's succesfully colided with player 2
+		
+		BallCollisionWall1(); //Keep checking collision
+		BallCollisionWall11();
+		
+			if (collisionThing == 1){ // Checking if it's on the top part of screen
+			move_ballplayer1();
+			move_balldown();
+		}
+		
+		if (collisionThing == 2){ // Checking if it's on the bottom part of screen
+			move_ballplayer1();
+			move_ballup();			
+		}
+	}
 	
 	display_image(0, bufferstate);
+	}
 	
+}	
+
+while (GAME == 2){ //Player 1 victory screen
+	
+	display_string(0,"");
+	display_string(1,"");
+	display_string(2,"PLAYER 1 GJ!!!!");
+	display_string(3,"");
+	display_update();
+	
+}
+
+while (GAME == 3){ //Player 2 victory screen
+	display_string(0,"");
+	display_string(1,"");
+	display_string(2,"PLAYER 2 GJ!!!!");
+	display_string(3,"");
+	display_update();
 }
 
 return 0;	
 }
-
-
-
-
-
